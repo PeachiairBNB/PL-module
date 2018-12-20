a303e2a16f8a59bc453bbc84891048deda8f514d
paul lee <paul.plk@gmail.com>
2018-12-19 19:58:23 -0500

    successfully inserted random images from my local computer to the photo property of my reviews collection

--- reviews-module/database/seeder.js
+++ reviews-module/database/seeder.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
-const insertAllDummyData = require('./index.js').insertAllDummyData;
-const images = require('../mock-data/images.js').images;
+const insertAllDummyData = require("./index.js").insertAllDummyData;
+const images = require("../mock-data/images.js").images;
 
 // console.log(images)
 
-fs.readFile('./mock-data/MOCK_DATA.json', 'utf-8', (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      let dummyData = JSON.parse(data);
+fs.readFile("./mock-data/MOCK_DATA.json", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    let dummyData = JSON.parse(data);
     //   console.log('length of dummy data', Object.keys(dummyData).length);
-      insertAllDummyData(dummyData);
+
+    //make array of images and randomly insert images to each photo property in dummydata
+    for (var key in dummyData) {
+        // console.log(key)
+        var randomImage = images[Math.floor(Math.random() * Math.floor(images.length))];
+        dummyData[key]['photo'] = randomImage;
     }
-  });
+    insertAllDummyData(dummyData);
+  }
+});
 
-  //in terminal, run
-  //node database/seeder.js
-
+//in terminal, run
+//node database/seeder.js
