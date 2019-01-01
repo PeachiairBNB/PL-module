5f0465621f12d66de8d30d04949386e495a2a573
paul lee <paul.plk@gmail.com>
2019-01-01 12:30:00 -0500

    completed Review component. Implemented Read More

---
+++ reviews-module/client/src/components/ReviewList.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import Review from './Review.jsx';
+
+class ReviewList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+      reviewCount: 0,
+      currentPage: 1
+    };
+  }
+//   componentDidMount() {
+//     this.setState({
+//       reviews: this.props.reviews,
+//       reviewCount: this.props.reviews.length
+//     });
+//   }
+
+  // changePage(pageNum) {
+
+  // }
+
+  render() {
+    return (
+      <div>
+        {/* reviews */}
+        <div>
+          {this.props.reviews.map(review => {
+            return <Review review={review} key={review.id}/>
+          })}
+        </div>
+        {/* pages */}
+        <div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReviewList;
+
+// {/* pages...when clicked, render 7 new reviews based on page # */}
+// <div>
+// <div className="_36rlri">
+//   <div style={{ marginBottom: 32 }}>
+//     <nav role="navigation" data-id="p3-ReviewsPagination">
+//       <span>
+//         <div>
+//           <ul className="_11hau3k" data-id="p3-ReviewsPagination">
+//             <li className="_8wtxgiq" data-id="page-1">
+//               <button
+//                 className="_1wtrbgoo"
+//                 type="button"
+//                 aria-label="Page 1, current page"
+//                 aria-busy="false"
+//               >
+//                 <div className="_e602arm">1</div>
+//               </button>
+//             </li>
+//             <li className="_1am0dt" data-id="page-2">
+//               <button
+//                 className="_1ip5u88"
+//                 type="button"
+//                 aria-label="Page 2"
+//                 aria-busy="false"
+//               >
+//                 <div className="_1bdke5s">2</div>
+//               </button>
+//             </li>
+//             <li className="_1am0dt" data-id="page-3">
+//               <button
+//                 className="_1ip5u88"
+//                 type="button"
+//                 aria-label="Page 3"
+//                 aria-busy="false"
+//               >
+//                 <div className="_1bdke5s">3</div>
+//               </button>
+//             </li>
+//             <li className="_1am0dt">
+//               <div className="_1oq38vn">
+//                 <div className="_1r804a6o">...</div>
+//               </div>
+//             </li>
+//             <li className="_1am0dt" data-id="page-29">
+//               <button
+//                 className="_1ip5u88"
+//                 type="button"
+//                 aria-label="Page 29"
+//                 aria-busy="false"
+//               >
+//                 <div className="_1bdke5s">29</div>
+//               </button>
+//             </li>
+//             <li className="_b8vexar">
+//               <button
+//                 className="_1ip5u88"
+//                 type="button"
+//                 aria-busy="false"
+//               >
+//                 <div className="_1yofwd5">
+//                   <div className="_1rltvky">
+//                     <svg
+//                       viewBox="0 0 18 18"
+//                       role="img"
+//                       aria-label="Next"
+//                       focusable="false"
+//                       style={{
+//                         height: "1em",
+//                         width: "1em",
+//                         display: "block",
+//                         fill: "currentColor"
+//                       }}
+//                     >
+//                       <path
+//                         d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z"
+//                         fillRule="evenodd"
+//                       />
+//                     </svg>
+//                   </div>
+//                 </div>
+//               </button>
+//             </li>
+//           </ul>
+//         </div>
+//       </span>
+//     </nav>
+//   </div>
+// </div>
+// </div>
--- reviews-module/client/src/index.jsx
+++ reviews-module/client/src/index.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './components/Search.jsx';
-import Reviews from './components/Reviews.jsx';
 import Ratings from './components/Ratings.jsx';
+import ReviewList from './components/ReviewList.jsx';
 import axios from 'axios';
 
 class ReviewsModule extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            reviews: []
+            reviews: [],
+            listingId: 1
         }
     }
     componentDidMount() {
         // render reviews for current listing
+        getReviews(this.state.listingId)
     }
 
+    getReviews(listingId) {
+        axios.get(`/api/items/${listingId}`)
+        .then(res => {
+            console.log(res.data);
+            this.setState({
+                reviews: res.data,
+                listingId: listingId
+            })
+        })
+        .catch(err => {
+            console.error(`error getting reviews from listing id ${listingId}`, err);
+        })
+    }
 
     render() {
         return(
             <div id='reviews'>
-            <div>
-                <section>
-                    {/* <Search /> */}
-                    <div>
-                        {/* <Ratings /> */}
-                        <Reviews />
-                        {/* Pages */}
-                    </div>
-                </section>
-            </div>
+                <div>
+                    <section>
+                        {/* <Search /> */}
+                        <div>
+                            {/* <Ratings /> */}
+                            <ReviewList reviews={this.state.reviews} />
+                            {/* Pages */}
+                        </div>
+                    </section>
+                </div>
             </div>
         )
     }
