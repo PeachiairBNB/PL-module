212f465fb16df214d503b5782c2545856cf5192f
paul lee <paul.plk@gmail.com>
2019-01-03 13:57:27 -0500

    changed app.use so I can render reviews for other listing ids

--- reviews-module/server/index.js
+++ reviews-module/server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('../database/index.js').db;
 const reviewsRoutes = require('./router.js');
+const path = require('path');
 
 const port = process.env.PORT || 3005;
 
@@ -10,7 +11,9 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client/dist'));
+// app.use(express.static(__dirname + '/../client/dist'));
+app.use("/:id", express.static(path.join(__dirname, "/../client/dist")));
+
 
 //get all the reviews for a specific listing id
 app.use('/', reviewsRoutes);
